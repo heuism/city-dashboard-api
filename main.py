61b277594daa80cdcbebb0a57242949f83c97d89
Hien Tran <hientran@Hiens-MacBook-Pro.local>
2025-06-01 11:24:38 +1000

    [quiz 8] updated code to add new city with temp

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from typing import List
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import time
 
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class CityAndTemp(BaseModel):
+    city: str
+    temp: int
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,15 +18,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+data: List[CityAndTemp] = [
+    CityAndTemp(city="Melbourne", temp=10),
+    CityAndTemp(city="Sydney", temp=20),
+    CityAndTemp(city="Ho Chi Minh", temp=30),
+    CityAndTemp(city="Adelaide", temp=10),
+    CityAndTemp(city="Queensland", temp=25),
+]
+
 @app.get("/cities")
 def get_cities(min: int = Query(0, description="Minimum value")):
     time.sleep(1.5)  # Simulate 1.5s delay
     
-    data = [
-      { "city": "Melbourne", "temp": 10 },
-      { "city": "Sydney", "temp": 20 },
-      { "city": "Ho Chi Minh", "temp": 30 },
-      { "city": "Adelaide", "temp": 10 },
-      { "city": "Queensland", "temp": 25 }
-    ];
-    return [city for city in data if city["temp"] >= min]
+    
+    return [city.dict() for city in data if city.temp >= min]
+
+@app.post("/cities")
+def add_cities(city: CityAndTemp):
+    time.sleep(1.5)  # Simulate 1.5s delay
+    data.append(city)
+    return {"received": city}
